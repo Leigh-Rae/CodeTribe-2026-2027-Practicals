64613c62004b37485529ac88bf2d15ee18bab75e
Leigh-Rae <leighraebooysen9@gmail.com>
2026-06-03 13:41:34 +0200

    PA0601-Objects

---
+++ PA0601.js
@@ -0,0 +1,9 @@
+let student = {
+    age : 18,
+    name : "Kayla" ,
+    course : "Tourism",
+    identity : "0506020203",
+    studentnumber: "06020804"
+}
+
+let students = [{ name:"Kayla" , age: 18 , course: " Tourism" , identity:"0506020203" , studentnumber:"06020804"}]
