813e4f20a35a08967cd8238c924a74197464ac5a
Leigh-Rae <leighraebooysen9@gmail.com>
2026-06-03 13:57:58 +0200

    Arrays

---
+++ PA0602.js
@@ -0,0 +1,25 @@
+const student = [{
+    age : 18,
+    name : "Kayla" ,
+    course : "Tourism",
+    identity : "0506020203",
+    studentnumber: "06020804"
+} ,
+ {
+    age : 20,
+    name : "Bronwyn" ,
+    course : "Art",
+    identity : "0602020326",
+    studentnumber: "060203089",
+},
+
+{
+    age : 19,
+    name : "Kaity" ,
+    course : "Business",
+    identity : "0403020526",
+    studentnumber: "060204078",
+}
+
+];
+console.log(students)
