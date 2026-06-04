4f6377755b621bdca80fe63d36b64b470ab1e092
Leigh-Rae <leighraebooysen9@gmail.com>
2026-06-04 09:41:39 +0200

    PA0801-Function-Purpose

---
+++ PA0801.js
@@ -0,0 +1,24 @@
+/* THE PURPOSE OF A FUNCTION 
+
+ The function's purpose is to act as a reusable block of code 
+that performs a specific, isolated task. It allows you to group
+ related operations together, give them
+ a name, and run that logic whenever needed without rewriting the same 
+ code multiple times.
+*/
+
+/* STEPS OF A WITHDRAWAL
+
+STEP 1: Insert your ATM card.
+STEP 2: Enter your pin.
+STEP 3: Select withdrawal.
+STEP 4: Choose the account type (savings or cheque).
+STEP 5: Enter the ammount to withdraw.
+STEP 6: Confirm the transaction.
+STEP 7: Collect the cash.
+STEP 8: Take your card and receipt.
+*/
+
+function withdrawal () {
+
+}
