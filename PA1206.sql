8ca3f2c1848145ee917d63cc39da6eac928f68a6
Leigh-Rae <leighraebooysen9@gmail.com>
2026-06-23 15:57:58 +0200

    Where

---
+++ PA1206.sql
@@ -0,0 +1,23 @@
+Create Database BrandsDB;
+Use BrandsDB;
+
+Create Table Brands(
+BrandID INT,
+BrandName VARCHAR(250),
+BrandCategory VARCHAR(100),
+CountryOfOrigin VARCHAR(50),
+Quantity INT
+);
+
+Show Tables;
+ 
+ Insert into Brands 
+ Values
+ ( 004, "Tier-Tier", "makeup", "Korea" , 50),
+ (002, "Adidas", "Clothes","USA", 10),
+ (003, "Nivea", "Skincare", "Australia", 34);
+ 
+ select *from Brands
+ WHERE quantity>20;
+
+desc Brands;
